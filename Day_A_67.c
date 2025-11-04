d1b99f30a7d0fc23173b75eb93792d79e17e72ae
Avinav-animeboy <akhilesh.thapa.at@gmail.com>
2025-11-04 10:12:00 +0530

    Create Day_A_67.c

---
+++ Day_A_67.c
@@ -0,0 +1,70 @@
+/*Q117: Write a program to take two sorted arrays of size m and n as input. Merge both the arrays such that the merged array is also sorted. Print the merged array.
+
+Sample Test Cases:
+Input 1:
+nums1 = [2,7,11,15] nums2 = [4,8,10]
+Output 1:
+2 4 7 8 10 11 15
+
+Input 2:
+nums1 = [1,2,7] nums2 = [9,10,17]
+Output 2:
+1 2 7 9 10 17
+
+Input 3:
+nums1 = [-10,-2,7] nums2 = [-3, -1, 7]
+Output 3:
+-10 -3 -2 -1 7 7
+
+*/
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    int nums1[100], nums2[100], merged[200];
+    int i = 0, j = 0, k = 0;
+
+    // Input first array
+    printf("Enter size of first array: ");
+    scanf("%d", &m);
+    printf("Enter %d sorted elements: ", m);
+    for (i = 0; i < m; i++) {
+        scanf("%d", &nums1[i]);
+    }
+
+    // Input second array
+    printf("Enter size of second array: ");
+    scanf("%d", &n);
+    printf("Enter %d sorted elements: ", n);
+    for (j = 0; j < n; j++) {
+        scanf("%d", &nums2[j]);
+    }
+
+    // Reset indexes
+    i = j = 0;
+    // Merge both arrays
+    while (i < m && j < n) {
+        if (nums1[i] < nums2[j]) {
+            merged[k++] = nums1[i++];
+        } else {
+            merged[k++] = nums2[j++];
+        }
+    }
+
+    // Copy remaining elements
+    while (i < m) {
+        merged[k++] = nums1[i++];
+    }
+
+    while (j < n) {
+        merged[k++] = nums2[j++];
+    }
+
+    // Print merged array
+    printf("Merged sorted array: ");
+    for (i = 0; i < k; i++) {
+        printf("%d ", merged[i]);
+    }
+printf("\n");
+    return 0;
+}
